3a72fbbab674c36d03e9c6531df750b2a922a073
Robinson Greig <robinsongreig@gmail.com>
2016-06-21 11:49:23 -0400

    change representative display format on letter form

--- lettersToGovernment/app/models/representative.rb
+++ lettersToGovernment/app/models/representative.rb
@@ -1,4 +1,8 @@
 class Representative < ActiveRecord::Base
 	has_many :subscriptions
 	has_many :letters, through: :subscriptions
+
+	def full_name
+	   "#{self.first_name} #{self.last_name} (#{self.state}-#{self.party.chars.first})"
+	end
 end
